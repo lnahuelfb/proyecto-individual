915375499d81da504710c4dbacaaf943879a0aca
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2021-12-26 17:07:13 -0300

    Terminado el backend

--- api/src/controller/Recipe.js
+++ api/src/controller/Recipe.js
@@ -1,180 +1,68 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable consistent-return */
-/* eslint-disable array-callback-return */
-/* eslint-disable radix */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-console */
 
-const { axios } = require('axios');
-
-const { API_KEY } = process.env;
 const { v4: uuidv4 } = require('uuid');
-const { Op } = require('sequelize');
 const { Recipe, Diet } = require('../db');
 
-const API = async () => {
-  try {
-    const APICall = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`);
+const addRecipe = async (req, res, next) => {
+  const {
+    title,
+    summary,
+    score,
+    healthy,
+    image,
+    steps,
+    diets,
+  } = req.body;
 
-    const data = APICall.data.results.map((recipe) => ({
-      title: recipe.title,
-      created: false,
-      Diets: recipe.diets.map((diet) => ({
-        name: diet,
-      })),
-      healthy: recipe.healthScore,
-      summary: recipe.summary,
-      image: recipe.image,
+  if (!title || !summary) {
+    return res
+      .status(400)
+      .send('Debes ingresar un titulo y su descripcion');
+  }
+
+  try {
+    const newRecipe = await Recipe.create({
       id: uuidv4(),
-      score: parseInt(recipe.spoonacularScore),
-      steps: recipe.analyzedInstructions.map((rec) => {
-        rec.steps.map((s) => {
-          s.step;
-        });
-      }).flat(2).join(''),
-    }));
-
-    return data;
-  } catch {
-    console.log('Error en el llamado a la API');
-    (err) => console.error(err);
-  }
-};
-
-const getRecipes = async (req, res, next) => {
-  const { name } = req.query;
-
-  if (!name) {
-    const allRecipesOnDB = await Recipe.findAll({
-      include: {
-        model: Diet,
-        through: {
-          attributes: [],
-        },
-      },
+      created: true,
+      title,
+      summary,
+      score: score || 0,
+      healthy: healthy || 0,
+      steps: steps || 'Esta receta no tiene paso a paso',
+      image: image || 'https://aeroclub-issoire.fr/wp-content/uploads/2020/05/image-not-found-300x225.jpg',
     });
 
-    if (allRecipesOnDB.length > 0) {
-      return res
-        .status(200)
-        .send(allRecipesOnDB);
+    if (diets.length) {
+      diets.map(async (diet) => {
+        try {
+          const dietDB = await Diet.findOne({
+            where: {
+              name: diet,
+            },
+          });
+          newRecipe.addDiet(dietDB);
+        } catch {
+          console.error('No se creó la nueva receta');
+          (err) => next(err);
+        }
+      });
     }
-
-    try {
-      const data = await API();
-      const recipesBulk = await Recipe.bulkCreate(data);
-
-      recipesBulk.map((recipe) => {
-        data.map((r) => {
-          if (r.id === recipe.id && r.Diets.length) {
-            r.Diets.map(async (diet) => {
-              try {
-                diet.name = `${diet.name.charAt(0).toUpperCase()} ${diet.name.slice(1)}`;
-
-                const dietOnDB = await Diet.findOne({
-                  where: {
-                    name: diet.name,
-                  },
-                });
-
-                await recipe.addDiet(dietOnDB);
-              } catch {
-                console.log('La dieta no se ha asociado');
-                (err) => next(err);
-              }
-            });
-          }
-        });
+    res
+      .status(201)
+      .json({
+        message: 'Receta crada con exito!',
       });
-
-      const AllrecipesOnBD = await Recipe.findAll({
-        include: {
-          model: Diet,
-          through: {
-            attributes: [],
-          },
-        },
-      });
-
-      return res
-        .status(200)
-        .send(AllrecipesOnBD);
-    } catch (err) {
-      console.log('No se ha podido crear y obtener las recetas');
-
+  } catch {
+    (err) => {
+      console.log('No se ha podido crear la receta');
       next(err);
-    }
-  } else {
-    const query = name.toLowerCase();
-
-    try {
-      const FilterRecipes = await Recipe.findAll({
-        where: {
-          title: {
-            [Op.like]: `%${query}%`,
-          },
-        },
-        include: {
-          model: Diet,
-          through: {
-            attributes: [],
-          },
-        },
-      });
-
-      return res
-        .status(200)
-        .send(FilterRecipes);
-    } catch {
-      console.log('Error al filtrar');
-      (err) => next(err);
-    }
-  }
-};
-
-const getRecipeById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    const recipe = await Recipe.findByPK(id, {
-      include: {
-        model: Diet,
-        through: {
-          attributes: [],
-        },
-      },
-    });
-
-    if (recipe) {
-      return res.status(200).json(recipe);
-    }
-
-    return res.status(404).json({
-      error: 'No se ha encontrado la receta',
-    });
-  } catch {
-    (err) => next(err);
-  }
-};
-
-const deleteRecipe = async (req, res, next) => {
-  const { id } = req.params;
-
-  try {
-    await Recipe.destroy({
-      where: {
-        id,
-      },
-    });
-
-    return res.send('Receta borrada con exito!');
-  } catch (err) {
-    next(err);
+    };
   }
 };
 
 module.exports = {
-  getRecipes,
-  getRecipeById,
-  deleteRecipe,
+  addRecipe,
 };
